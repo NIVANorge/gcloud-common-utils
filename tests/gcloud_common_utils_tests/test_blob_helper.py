72648d5aa73cbfce286e2f43459127705c8fd3a4
RmStorm <roaldstorm@gmail.com>
2020-09-16 09:57:39 +0200

    Added a test to to local_blob_helper upload and download
    Also slightly changed the upload function to not recreate directory
    structures in the temp bucket. Instead it adds a microsecond timestamp
    to each tempfile before moving.

--- tests/gcloud_common_utils_tests/test_blob_helper.py
+++ tests/gcloud_common_utils_tests/test_blob_helper.py
@@ -1,3 +1,33 @@
-def test_blob_helper():
-    # TODO: add tests
-    pass
+import os
+import shutil
+import logging
+from io import BytesIO
+import random
+
+import pytest
+
+from gcloud_common_utils import blob_helper_local
+
+
+@pytest.fixture
+def make_and_delete_temp_folder(monkeypatch, caplog):
+    caplog.set_level(logging.INFO)
+    tmp_folder = f"/tmp/stream-data{random.randint(1,1000)}"
+    monkeypatch.setenv("LOCAL_STORAGE_PATH", tmp_folder)
+    os.makedirs(tmp_folder, exist_ok=True)
+    yield
+    shutil.rmtree(tmp_folder)
+
+
+def test_local_blob_helper_upload_and_download(make_and_delete_temp_folder):
+    byte_string = b'yada yada yada\nmore yadas!!\n'
+
+    with BytesIO() as some_bytes:
+        some_bytes.write(byte_string)
+        some_bytes.seek(0)
+        blob_helper_local.upload_blob('a_test_bucket', 'a_test_file.txt', some_bytes)
+
+    with BytesIO() as receiving_buffer:
+        blob_helper_local.download_blob('a_test_bucket', 'a_test_file.txt', receiving_buffer)
+        receiving_buffer.seek(0)
+        assert receiving_buffer.read() == byte_string
